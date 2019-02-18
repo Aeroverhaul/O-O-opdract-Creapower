541c730ca7aaef19b60427a6e0b348b8d2de1877
Alex Rietkerk <47634702+Jal3xR@users.noreply.github.com>
2019-02-18 13:32:55 +0100

    Add files via upload

---
+++ Firebase_config.js
@@ -0,0 +1,10 @@
+export default {
+    config: {
+        apiKey: "AIzaSyCWnPFIsFemyyuM-HsIzLvQefqBds6ctI0",
+	    authDomain: "creapower-1dc33.firebaseapp.com",
+	    databaseURL: "https://creapower-1dc33.firebaseio.com",
+	    projectId: "creapower-1dc33",
+	    storageBucket: "creapower-1dc33.appspot.com",
+	    messagingSenderId: "407831769181"
+    }
+}
